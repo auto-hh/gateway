c5e28f803e4df061bf68e0643b7c18617807025e
Авдеев Матвей Анатольевич <maavdeev@edu.hse.ru>
2026-03-03 15:42:02 +0300

    feat: created implementation of Repository interface for Redis storage

--- internal/repository/redis/redis.go
+++ internal/repository/redis/redis.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -12,7 +13,28 @@ type Repo struct {
 	client *redis.Client
 }
 
-func NewClient(ctx context.Context, addr string, dbName int, username, password string, maxRetries int, dialTimeout, readTimeout, writeTimeout time.Duration) (*redis.Client, error) {
+func NewRepo(ctx context.Context, addr string, dbName int, username, password string, maxRetries int, dialTimeout, readTimeout, writeTimeout time.Duration) *Repo {
+	client, err := NewClient(ctx, addr, dbName, username, password, maxRetries, dialTimeout, readTimeout, writeTimeout)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("redis.NewRepo: %v", err))
+	}
+
+	return &Repo{
+		client: client,
+	}
+}
+
+func NewClient(
+	ctx context.Context,
+	addr string,
+	dbName int,
+	username,
+	password string,
+	maxRetries int,
+	dialTimeout,
+	readTimeout,
+	writeTimeout time.Duration,
+) (*redis.Client, error) {
 	db := redis.NewClient(&redis.Options{
 		Addr:         addr,
 		DB:           dbName,
@@ -30,4 +52,16 @@ func NewClient(ctx context.Context, addr string, dbName int, username, password 
 	}
 
 	return db, nil
+}
+
+func (r *Repo) Get(ctx context.Context, key string) (string, error) {
+	return r.client.Get(ctx, key).Result()
+}
+
+func (r *Repo) Set(ctx context.Context, key string, value string) error {
+	return r.client.Set(ctx, key, value, 0).Err()
+}
+
+func (r *Repo) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,9 @@
 package repository
 
-type Repo interface {
-	Get()
-	Set()
-	Delete()
-	Init()
+import "context"
+
+type Repository interface {
+	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, value string) error
+	Delete(ctx context.Context, key string) error
 }
